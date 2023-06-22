4ec32c8c13c4480ee088fbaec41db4dc4e63abe7
Gianni <giannicimmino1996@gmail.com>
2023-06-22 10:40:55 +0200

    Domande D1

---
+++ index.js
@@ -0,0 +1,19 @@
+/*Cos'è un algoritmo?*/
+/*Un algoritmo è un'insieme di istruzioni o regole che servono a dividere il problema in più parti. 
+Gli algoritmi sono fondamentali per risolvere problemi, poichè il computer non riesce ad elaborarli facilmente,
+quindi si usano istruzioni semplici e precise per far svolgere determinate operazioni.*/
+
+
+
+/*Cos'è una variabile?*/
+/*Una variabile in poche parole è una scatola che contiene tutto quello che ci si mette all'interno e la si può usare ogni volta
+che si vuole. Nell'informatica le variabili devono essere dichiarate e c'è ne sono di tanti tipi.
+Le variabili possono contenere numeri e lettere in base a ciò che serve e possono essere riutilizzate per tutto il programma
+richiamandole ogniqualvolta se ne ha bisogno.  */
+
+
+
+/*Undefined e null sono la stessa cosa.*/
+/*Undefined e null sono sono due valori che rappresentano la mancanza di un valore o un valore vuoto.
+Nel caso di undefined, la variabile che abbiamo dichiarato è vuota, ciò vuol dire che non è stata assegnato nessun valore.
+Nel caso di null, la variabile che abbiamo dichiarato è stata intenzionalmente dichiarata nulla, quindi la scatola è vuota. */
